9496a8ea27993c39bf024d2ce2a16547dd05f8c1
GustavoD <gduartemenezes@gmail.com>
2020-04-23 17:58:37 -0300

    Used react-router-dom to create the first app routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
-
+import Routes from "./routes";
+import "./pages/global.css";
 function App() {
-  return <h1>Hello</h1>;
+  return <Routes />;
 }
 
 export default App;
