c3a530d50a5ebc10454c82a30fd430f28d559fea
GustavoD <gduartemenezes@gmail.com>
2020-04-25 14:55:59 -0300

    Added routes to Incident and Profile components

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,13 +3,16 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Logon from "./pages/Logon";
 import Register from "./pages/Register";
-
+import Incident from "./pages/Incident";
+import Profile from "./pages/Profile";
 export default function Router() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Logon} />
         <Route path="/register" component={Register} />
+        <Route path="/incidents/new" component={Incident} />
+        <Route path="/profile" component={Profile} />
       </Switch>
     </BrowserRouter>
   );
