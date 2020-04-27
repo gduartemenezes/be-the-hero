459ab0604228ee41c5a2d6ad12a926487409ed0f
GustavoD <gduartemenezes@gmail.com>
2020-04-27 08:12:04 -0300

    Installed cors dependecy to connect ract and node apps

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cors = require("cors");
+
 const ngoRouter = require("./routers/ngoRoutes");
 const incidentsRouter = require("./routers/incidentsRoutes");
 const profileRouter = require("./routers/profileRoutes");
@@ -6,6 +8,7 @@ const sessionRouter = require("./routers/sessionRoutes");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/api/v1/ngos", ngoRouter);
 app.use("/api/v1/incidents", incidentsRouter);
 app.use("/api/v1/profile", profileRouter);
