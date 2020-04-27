e49a8687ccfe1f89dbb204d98fa2574005fd2768
GustavoD <gduartemenezes@gmail.com>
2020-04-27 08:26:50 -0300

    Connect Incident Component with API

--- frontend/src/pages/Incident/index.js
+++ frontend/src/pages/Incident/index.js
@@ -1,28 +1,73 @@
 //dependencies
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import api from "../../services/api";
+
 //frontend styling
 import { FiArrowLeft } from "react-icons/fi";
 import "./styles.css";
 import logo from "../../assets/logo.png";
 
 export default function CreateIncident() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [value, setValue] = useState("");
+  const ngoId = localStorage.getItem("ngoId");
+
+  const history = useHistory();
+
+  async function handleNewIncident(event) {
+    event.preventDefault();
+
+    const data = {
+      title,
+      description,
+      value,
+    };
+
+    try {
+      await api.post("incidents", data, {
+        headers: {
+          Authorization: ngoId,
+        },
+      });
+    } catch (error) {
+      console.log("Something went wrong");
+      console.log(error);
+    }
+  }
+
   return (
     <div className="incident-container">
       <section className="static">
         <img src={logo} alt="Logo" />
         <h1>Create a new case</h1>
         <p>Describe it with details. This way our heroes can help you better</p>
-        <Link className="link">
+        <Link className="link" to="/profile">
           <FiArrowLeft /> Back to Profile page
         </Link>
       </section>
 
       <section className="form">
-        <form>
-          <input type="text" placeholder="Title" />
-          <input type="text" placeholder="Description" />
-          <input type="text" placeholder="Value" />
+        <form onSubmit={handleNewIncident}>
+          <input
+            type="text"
+            placeholder="Title"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Description"
+            value={description}
+            onChange={(event) => event.target.value}
+          />
+          <input
+            type="text"
+            placeholder="Value"
+            value={value}
+            onChange={(event) => setValue(event.target.value)}
+          />
           <button className="btn btn-register" type="submit">
             Add case
           </button>
